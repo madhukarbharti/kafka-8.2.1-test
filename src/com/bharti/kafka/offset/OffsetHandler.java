d2d12cfce26672b0d231f6296b8c4dab6bcef3b9
madhukar.bharti <madhukar.bharti@zohocorp.com>
2015-03-30 14:57:58 +0530

    High Level consumer: Example for commiting to offset topic

---
+++ src/com/bharti/kafka/offset/OffsetHandler.java
@@ -0,0 +1,148 @@
+/**
+ * 
+ */
+package com.bharti.kafka.offset;
+
+/**
+ * @author Madhukar
+ *
+ */
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import kafka.javaapi.ConsumerMetadataResponse;
+import kafka.javaapi.OffsetCommitRequest;
+import kafka.javaapi.OffsetCommitResponse;
+import kafka.javaapi.OffsetFetchRequest;
+import kafka.javaapi.OffsetFetchResponse;
+import kafka.javaapi.OffsetRequest;
+import kafka.javaapi.OffsetResponse;
+import kafka.javaapi.consumer.SimpleConsumer;
+import kafka.network.BlockingChannel;
+import kafka.api.ConsumerMetadataRequest;
+import kafka.api.PartitionOffsetRequestInfo;
+import kafka.cluster.Broker;
+import kafka.common.ErrorMapping;
+import kafka.common.OffsetAndMetadata;
+import kafka.common.OffsetMetadataAndError;
+import kafka.common.TopicAndPartition;
+
+
+public class OffsetHandler {
+
+		
+	private static final Logger LOGGER = Logger.getLogger(OffsetHandler.class.getName());	
+	
+	public static long readLastOffset(SimpleConsumer consumer, String topic, int partition, long whichTime) {
+        Map<TopicAndPartition, PartitionOffsetRequestInfo> requestInfo = new HashMap<TopicAndPartition, PartitionOffsetRequestInfo>();
+        requestInfo.put(new TopicAndPartition(topic, partition), new PartitionOffsetRequestInfo(whichTime, 1));
+        OffsetResponse response = consumer.getOffsetsBefore(new OffsetRequest(requestInfo, kafka.api.OffsetRequest.CurrentVersion(), consumer.clientId()));
+        if (response.hasError()) {
+            LOGGER.log(Level.INFO, "Error fetching data Offset Data the Broker. Reason: " + response.errorCode(topic, partition));
+            return 0;
+        }
+        long[] offsets = response.offsets(topic, partition);
+        return offsets[0];
+    }
+	
+	public static long getConsumedOffset(String groupId) {
+		long offset = -1;
+
+		BlockingChannel channel = new BlockingChannel("localhost", 9092,
+				BlockingChannel.UseDefaultBufferSize(),
+				BlockingChannel.UseDefaultBufferSize(), 5000 /*
+															 * read timeout in
+															 * millis
+															 */);
+		channel.connect();
+		channel.send(new ConsumerMetadataRequest(groupId, (short) 1, 0,
+				"cm-requester"));
+
+		ConsumerMetadataResponse cmr = ConsumerMetadataResponse
+				.readFrom(channel.receive().buffer());
+
+		Broker broker = null;
+		if (cmr.errorCode() == ErrorMapping.NoError()) {
+			System.out
+					.println("No Error while getting consumer metadata response");
+			broker = cmr.coordinator();
+		}
+		if (broker == null) {
+			System.out.println("No coordinator broker available");
+			return -1;
+		}
+
+		TopicAndPartition part0 = new TopicAndPartition("TestQ", 0);
+		List<TopicAndPartition> reqInfo = new ArrayList<TopicAndPartition>();
+		reqInfo.add(part0);
+
+		OffsetFetchRequest oreq = new OffsetFetchRequest(groupId, reqInfo, (short) 1, 0, "offsetRequester");
+		channel.send(oreq.underlying());
+
+		OffsetFetchResponse resp = OffsetFetchResponse.readFrom(channel.receive().buffer());
+		OffsetMetadataAndError ome = resp.offsets().get(part0);
+		if(ome.error() == ErrorMapping.NoError()){
+			offset = ome.offset();
+			System.out.println("Offset::" + resp.offsets().get(part0).offset());
+		}else{
+			System.out.println("Error code is::"+ome.error());
+			offset = -1;
+		}
+		return offset;
+	}
+	
+	public static int commitOffset(String brokerConnect, int port, String groupId, String topic, int partition, long offset) {
+		
+		TopicAndPartition partitionInfo = new TopicAndPartition(topic, partition);
+        Map<TopicAndPartition, OffsetAndMetadata> offsetMap = new LinkedHashMap<TopicAndPartition, OffsetAndMetadata>();
+        
+		BlockingChannel channel = new BlockingChannel(brokerConnect, port,
+				BlockingChannel.UseDefaultBufferSize(),
+				BlockingChannel.UseDefaultBufferSize(), 5000);
+		channel.connect();
+		
+		channel.send(new ConsumerMetadataRequest(groupId, (short) 1, 0, "cm-requester"));
+
+		ConsumerMetadataResponse cmr = ConsumerMetadataResponse.readFrom(channel.receive().buffer());
+
+		Broker broker = null;
+		if (cmr.errorCode() == ErrorMapping.NoError()) {			
+			broker = cmr.coordinator();
+		}
+		if (broker == null) {
+			System.out.println("No coordinator broker available");
+			
+		}
+
+		long currentTime = System.currentTimeMillis();
+        offsetMap.put(partitionInfo, new OffsetAndMetadata(offset, "commiting-"+offset, currentTime));
+        
+		OffsetCommitRequest commitReq = new OffsetCommitRequest(groupId, offsetMap, 0, "offset-commit-client", (short)1);
+		
+		channel.send(commitReq.underlying());
+		
+		OffsetCommitResponse commitResp = OffsetCommitResponse.readFrom(channel.receive().buffer());
+		
+		if(commitResp.errorCode(partitionInfo) == ErrorMapping.NoError()){			
+			System.out.println("Offset Commited successfully, "+offset);			
+		}
+		return commitResp.errorCode(partitionInfo);
+	}
+	
+	public static void main(String[] args) {
+		/*String brokerConnect = "localhost";
+		int port = 9092;
+		String topic = "TestQ";
+		String groupId = "TestGroup5";*/		
+		getConsumedOffset("TestConsumerGroup1");
+	}
+	
+}
---
+++ src/com/bharti/kafka/offset/TestConsumer.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.bharti.kafka.offset;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.MessageAndMetadata;
+
+/**
+ * @author Madhukar
+ *
+ */
+public class TestConsumer {
+
+	
+	private static ConsumerConfig createConsumerConfig(String group){
+		Properties props = new Properties();
+		props.put("zookeeper.connect", "localhost:2181");
+		props.put("group.id", group);
+		props.put("zookeeper.sessiontimeout.ms", "6000");
+		props.put("zookeeper.connection.timeout.ms", "12000");
+		props.put("zookeeper.synctime.ms", "200");
+	
+		props.put("auto.offset.reset", "smallest");
+		props.put("auto.commit.enable", "false");
+		props.put("offsets.storage", "kafka");
+		props.put("dual.commit.enabled", "false");
+		return new ConsumerConfig(props);
+	}
+	
+	private static void startConsuming(String topic, String groupId){
+		ConsumerConnector consumer = kafka.consumer.Consumer.createJavaConsumerConnector(createConsumerConfig(groupId));
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(topic, new Integer(1));
+		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+		KafkaStream<byte[], byte[]> stream =  consumerMap.get(topic).get(0);
+		ConsumerIterator<byte[], byte[]> it = stream.iterator();
+		System.out.println("Hi");		
+		while(it.hasNext()){
+			MessageAndMetadata<byte[], byte[]> md = it.next();
+			byte[] arr = (byte[]) md.message();
+			OffsetHandler.commitOffset("localhost", 9092, groupId, topic, md.partition(), md.offset());
+			System.out.println("Received :::" + new String(arr));		
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		String topic = "TestQ";
+		startConsuming(topic, "TestConsumerGroup2");
+	}
+
+}
